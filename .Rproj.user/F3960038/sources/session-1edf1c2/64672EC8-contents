c8207cc405c956c32a2eda75195f1ec1b31de691
Esben Lykke <sesben@hotmail.com>
2023-08-15 14:46:13 +0200

    initial commit

---
+++ .Rproj.user/F3960038/sources/session-1edf1c2/64672EC8-contents
@@ -0,0 +1,117 @@
+#!/usr/bin/env Rscript
+
+# List of required packages
+required_packages <- c(
+  "tidyverse", "tidymodels", "furrr", "slider", "arrow",
+  "read.cwa", "glue", "lubridate", "signal", "moments", "xgboost"
+)
+
+# Check and install missing packages
+missing_packages <- setdiff(required_packages, installed.packages()[, "Package"])
+
+if (length(missing_packages) > 0) {
+  install.packages(missing_packages, dependencies = TRUE)
+} else {
+  cat("Lets go!\n")
+}
+
+# load packages
+suppressMessages(library(tidyverse))
+suppressMessages(library(tidymodels))
+suppressMessages(library(furrr))
+suppressMessages(library(slider))
+suppressMessages(library(arrow))
+suppressMessages(library(GGIRread))
+suppressMessages(library(glue))
+suppressMessages(library(lubridate))
+suppressMessages(library(signal))
+suppressMessages(library(moments))
+
+args <- R.utils::commandArgs(trailingOnly = TRUE)
+
+# epoch_length <- as.integer(args[1]) # epoch length in seconds
+cwa_path <- args[1] # path to temp cwa files
+
+# seconds
+epoch_length <- 30
+
+cwa_files_basenames <-
+  list.files(cwa_path, full.names = F) %>%
+  str_subset("cwa")
+
+cwa_files <- 
+  str_c(cwa_path, cwa_files_basenames)
+
+temp_files <-
+  str_replace_all(str_c(cwa_path, "temp/", cwa_files_basenames), ".cwa", ".parquet")
+
+# The mean_crossing_rate function takes the signal as input and calculates
+# the mean crossing rate. It counts the number of times the signal crosses
+# the mean value and divides it by the length of the signal.
+mean_crossing_rate <- function(signal) {
+  crossings <- sum(abs(diff(sign(signal))) / 2)
+  crossing_rate <- crossings / length(signal)
+  return(crossing_rate)
+}
+
+cat("Applying filter and calculating the first round of features. This will take some time...\n")
+
+process_cwa <- function(cwa_file, temp_file) {
+  glue::glue("Extracting features for {cwa_file}...\n")
+  
+  raw <-
+    GGIRread::readAxivity(cwa_file, start = 1, end = 14400)
+    
+  acc <- 
+    raw$data %>% 
+    as_tibble() %>% 
+    mutate(
+      datetime = as.POSIXct(time, origin = "1970-01-01"),
+      .before = 1
+    )
+
+  sf <- 
+    raw$header$frequency
+  
+  # Create 4th order Butterworth low-pass 5 Hz filter
+  bf <- signal::butter(4, 5 / (sf / 2), type = "low")
+
+  out <- 
+    acc %>% 
+    mutate(
+      epoch = floor_date(datetime, "30 seconds"),
+      noon_day = day(epoch - hours(12)),
+      month = month(epoch)) %>% 
+    group_by(epoch, noon_day, month) %>% 
+    reframe(
+      id = str_extract(cwa_files[1], "\\d{10}"),
+      id = str_remove(id, "^0+"),
+      sensor_code = str_extract(cwa_files[1], "\\d{5}"),
+      across(x:z, ~ signal::filtfilt(bf, .x)),
+      across(x:temp, list(
+        mean = mean,
+        sd = sd
+      )),
+      weekday = wday(datetime, label = FALSE, week_start = 1),
+      incl = 180 / pi * acos(y_mean / sqrt(x_mean^2 + y_mean^2 + z_mean^2)),
+      theta = 180 / pi * asin(z_mean / sqrt(x_mean^2 + y_mean^2 + z_mean^2)),
+      vector_magnitude = sqrt(x_mean^2 + y_mean^2 + z_mean^2),
+      across(c(x, y, z), list(
+        crossing_rate = mean_crossing_rate,
+        skewness = skewness,
+        kurtosis = kurtosis
+      ))
+    ) %>% 
+    group_by(epoch) %>% 
+    slice(1) %>% 
+    ungroup() %>% 
+    write_parquet(temp_file)
+}
+
+plan(multisession, workers = 5)
+
+future_walk2(cwa_files, temp_files, ~ process_cwa(.x, .y),
+  .options = furrr_options(seed = 123), .progress = TRUE
+)
+
+plan(sequential)
---
+++ merge_and_extract_remaining_features.R
@@ -0,0 +1,97 @@
+#!/usr/bin/env Rscript
+
+suppressMessages(library(tidyverse))
+suppressMessages(library(tidymodels))
+suppressMessages(library(furrr))
+suppressMessages(library(arrow))
+suppressMessages(library(R.utils))
+suppressMessages(library(glue))
+suppressMessages(library(slider))
+
+
+conflicted::conflict_prefer("filter", "dplyr")
+conflicted::conflict_prefer("lag", "dplyr")
+
+args <- commandArgs(trailingOnly = TRUE)
+
+temp_path <- args[1] # directory with temp parquet files
+dest <- args[2] # destination filename
+
+xgb_in_bed <-
+  read_rds("models/in_bed_median5_xgboost_30_sec_epoch_fit.rds")
+
+xgb_sleep <-
+  read_rds("models/sleep_5_min_median_xgboost_30_sec_epochs_fit.rds")
+
+cat("Calculating remaining features\n")
+
+basenames <- list.files(temp_path, ".parquet", full.names = FALSE)
+
+temp_files <-
+  list.files(temp_path, ".parquet", full.names = TRUE)
+
+output_files <-
+  file.path(dest, basenames)
+
+
+process_temp_parquet <- function(temp_file, output_file) {
+  cat(glue("Processing {str_remove(temp_file, temp_path)}...\n\n"))
+  tbl <-
+    read_parquet(temp_file) %>%
+    mutate(age = 8) %>% # TODO: remove this fake age when real data is available
+    rowwise() %>%
+    mutate(
+      sd_max = max(c(x_sd, y_sd, z_sd))
+    ) %>%
+    group_by(id, noon_day, month) %>%
+    mutate(
+      clock_group = if_else((hms::as_hms(epoch) > lubridate::hms("19:00:00") |
+        hms::as_hms(epoch) < lubridate::hms("10:00:00")), 1, 0),
+      across(c(x, y, z), list(sd_long = ~ slider::slide_dbl(.x, sd, .after = 30))),
+      across(x_sd_long:z_sd_long, ~ replace_na(.x, mean(.x, na.rm = TRUE))),
+      across(c(incl, theta, temp_mean, x_sd, y_sd, z_sd), list(
+        lag_1min = ~ lag(.x, 2, default = mean(.x)), # Value from 1 minute ago
+        lag_5min = ~ lag(.x, 10, default = mean(.x)), # Value from 5 minutes ago
+        lag_30min = ~ lag(.x, 60, default = mean(.x)), # Value from 30 minutes ago
+        lead_1min = ~ lead(.x, 2, default = mean(.x)), # Value from 1 minute in the future
+        lead_5min = ~ lead(.x, 10, default = mean(.x)), # Value from 5 minutes in the future
+        lead_30min = ~ lead(.x, 60, default = mean(.x)) # Value from 30 minutes in the future
+      )),
+      .after = sd_max
+    ) %>%
+    # Group the data by id, noon_day, month, and clock_group
+    group_by(id, noon_day, month, clock_group) %>%
+    # Create new features for clock_proxy_cos and clock_proxy_linear
+    mutate(
+      clock_proxy_cos = if_else(clock_group == 1,
+        cos(seq(-(pi / 2), pi / 2, length.out = n())), 0
+      ),
+      clock_proxy_linear = if_else(clock_group == 1,
+        seq(0, 1, length.out = n()), 0
+      )
+    ) %>%
+    ungroup()
+
+  in_bed_extract <-
+    xgb_in_bed %>%
+    augment(tbl) %>%
+    mutate(.pred_class = slide_dbl(as.numeric(.pred_class) - 1, median, .after = 15, .before = 15)) %>%
+    group_by(id, noon_day, month) %>%
+    group_modify(~ .x %>%
+      # Filter rows where row_number is within the in_bed_filtered range
+      filter(row_number() >= min(row_number()[.data$.pred_class == 1]) &
+        row_number() <= max(row_number()[.data$.pred_class == 1]))) %>%
+    ungroup() %>%
+    rename(pred_in_bed = .pred_class, pred_in_bed_yes = .pred_1, pred_in_bed_no = .pred_0)
+
+  combined_predictions <-
+    xgb_sleep %>%
+    augment(in_bed_extract) %>%
+    rename(pred_asleep = .pred_class, pred_asleep_yes = .pred_1, pred_asleep_no = .pred_0) %>%
+    write_parquet(output_file)
+}
+
+
+walk2(temp_files, output_files, ~ process_temp_parquet(.x, .y), .progress = TRUE)
+
+cat(glue("\n{length(warnings())} warnings generated. Warnings are typically generated when no in-bed time is found on a given night...\n"))
