e28b1fa27a011f70cff5ce27a0b652514a102986
Esben Lykke <sesben@hotmail.com>
2023-08-23 11:02:13 +0200

    update r_dependencies.R

--- r_dependencies.R
+++ r_dependencies.R
@@ -1,16 +1,23 @@
 #!/usr/bin/env Rscript
 
-# List of required packages
+# List of required packages from CRAN
 required_packages <- c(
-  "tidyverse", "tidymodels", "furrr", "slider", "arrow",
-  "read.cwa", "glue", "lubridate", "signal", "moments", "xgboost"
+  "tidyverse", "tidymodels", "slider", "arrow", "glue", "lubridate", "signal", "moments", "xgboost"
 )
 
-# Check and install missing packages
+# Check and install missing CRAN packages
 missing_packages <- setdiff(required_packages, installed.packages()[, "Package"])
 
 if (length(missing_packages) > 0) {
   install.packages(missing_packages, dependencies = TRUE)
+}
+
+# Check and install GGIRread from GitHub if not installed
+if (!"GGIRread" %in% installed.packages()[, "Package"]) {
+  if (!"remotes" %in% installed.packages()[, "Package"]) {
+    install.packages("remotes")
+  }
+  remotes::install_github("jbrond/GGIRread")
 } else {
   cat("Lets go!\n")
 }
